3358b18681a0d4d513970a9b0b8b0fee799218bb
Kamil Naglik <k.naglik@donedeliveries.com>
2024-09-29 09:22:56 +0200

    basic modals and menu styling

--- Scripts/EventResolvedModal.cs
+++ Scripts/EventResolvedModal.cs
@@ -6,14 +6,19 @@ public partial class EventResolvedModal : Node2D
 	public Label Title { get; set; }
 	public Label Description { get; set; }
 	private Button closeButton;
+
+	private Panel panel;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Title = GetNode<Label>("VBoxContainer/Title");
-		Description = GetNode<Label>("VBoxContainer/Description");
+		Title = GetNode<Label>("Panel/VBoxContainer/Title");
+		Description = GetNode<Label>("Panel/VBoxContainer/Description");
 		
-		closeButton = GetNode<Button>("VBoxContainer/CloseButton");
+		closeButton = GetNode<Button>("Panel/VBoxContainer/CloseButton");
 		closeButton.Pressed += OnCloseButtonPressed;
+
+		panel = GetNode<Panel>("Panel");
+		panel.Size = new Vector2(GetViewport().GetVisibleRect().Size.X, GetViewport().GetVisibleRect().Size.Y);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
